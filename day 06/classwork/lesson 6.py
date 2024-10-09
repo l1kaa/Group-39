cb2cb098646e46eb7de14429644ffe6ec6f6ebe2
l1kaa <chxikvadzelikuna1@gmai.com>
2024-10-09 15:56:33 +0400

    commit

---
+++ day 06/classwork/lesson 6.py
@@ -0,0 +1,15 @@
+age = int(input("please enter your age:"))
+print(age // 10)
+
+
+age = int(input("please enter your age:"))
+print(type(age))
+age2 = (float(age))
+print(age2)
+
+name = (input("please enter your name:"))
+number = int(input("please enter a number:"))
+print(name * number)
+
+# implicit არის როდესაც მონაცემთა ტიპი ავტომატურად იცვლება.
+# explicit არის როდესაც მონაცემთა ტიპს საკუთარი ხელით ვცვლით მაგ int() str() float() საშუალებით
---
+++ day 06/homework/lesson 6.py
@@ -0,0 +1,35 @@
+# task 1:
+age = input("please enter your age: ")
+users_age_in_10_years = int(age) + 10
+print("your age in 10 years will be:" ), print(users_age_in_10_years)
+float(users_age_in_10_years)
+print(type(users_age_in_10_years))
+
+# task 2:
+surname = input("please enter your surname:")
+print(surname * 100)
+
+
+# task 3:
+print("out" + "come")
+print("back" + "space")
+print("master" + "piece")
+print("never" + "the" + "less")
+print("what" + "so" + "ever") 
+
+# task 4:
+#integer-ს ვიყენებთ, რათა მისი საშუალებით შევასრულოთ სხვადასხვა მათემატიკური ოპერაციები.
+#string-ის საშუალებით ვახდენთ ცვლადებში ცვლადის დასახელების შესაბამის მნიშვნელობების შენახვას, ამასთანავე მისი საშუალებით მიმატების ოპერატორის გამოყენებით ვაერთებთ(კონკატინაციას ვახდენთ) ორ ან ორზე მეტ სიტყვას,რასაც ვერ შეძლებთ სხვა მონაცემთა ტიპების საშუალებით.
+#float-ითაც ისევე შეგვიძლია მათემატიკური ოპერაციების ჩატარება როგორც integer-ით.
+#boolean გვეხმარება დავადგინოთ მოცემული ესა თუ ის ინფორმაცია შეესაბამება თუ არა სიმართლეს ან სიცრუეს. (True და False)
+
+# task 5:
+movie = "Fight club"
+year = 1999
+budget = 63000000.500
+is_a_masterpiece = True
+
+print(type(movie))
+print(type(year))
+print(type(budget))
+print(type(is_a_masterpiece))
