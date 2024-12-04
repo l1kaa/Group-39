4774b2846427af4a77e2cf357c13fe2d33214be3
l1kaa <chxikvadzelikuna1@gmai.com>
2024-12-04 11:52:02 +0400

    commit

--- day 14/homework/test.py
+++ day 14/homework/test.py
@@ -1,5 +1,5 @@
-numbers = [1000, 1500, 2000, 2500, 3000, 3500, 4000, 4500, 5000, 5500, 6000, 6500, 7000, 7500, 8000, 8500, 9000, 9500, 10000]
-print(numbers[:5]) #პირველი ხუთი რიცხვი
-print(numbers[::3]) #ყოველი მესამე რიცხვი
-new_numbers = numbers[:5] + numbers[::3]
-print(len(new_numbers))
+numss = input("please enter 10 numbers using columns:")
+listt = [numss]
+print(listt)
+# listt = [1,2,3,4,5,6,7,8,9,10]
+print(listt[0:4:1])
