8e817bd06496fa7208079166ec5d50a391e5937f
l1kaa <chxikvadzelikuna1@gmai.com>
2025-01-10 23:04:35 +0400

    ★

---
+++ day 60/classwork/lesson60.py
@@ -0,0 +1,7 @@
+# 1st task: codewars - done.
+# 2nd task: შექმნით სია 5 ელემენტით, შემდეგ კი დაპრინტეთ ამ ელემენტების ჯამი, for loop.
+list = [700,450,67,13,56]
+count = 0
+for i in list:
+    count+= i
+print(count)
